a563b3877d9a9657185468189b92309878b951be
rodrigo <rodrigorivasmiranda@gmail.com>
2021-06-15 17:15:52 -0500

    actualizado documentos

---
+++ asssets/js/script.js
@@ -0,0 +1,30 @@
+var dato1 = 24;
+var dato2 = "Holaaaaaaa";
+var dato3 = "false";
+var dato4 = "5.564636244657989785453425";
+var dato5 = true;
+var dato6 = null;
+var dato7 = "Hoy es un lindo dia";
+var dato8 = 829;
+var dato9 = "null";
+var dato10 = false;
+
+
+var suma = parseInt(dato4) + dato8;
+
+
+console.log ("Suma", suma);
+console.log("ParseFloat:", parseFloat(dato4).toPrecision(30));
+
+
+console.log(typeof(dato1));
+console.log(typeof(dato2));
+console.log(typeof(dato3));
+console.log(typeof(parseInt(dato4)), parseFloat(dato4));
+console.log(typeof(dato5));
+console.log(typeof(dato6));
+console.log(typeof(dato7));
+console.log(typeof(dato8));
+console.log(typeof(dato9));
+console.log(typeof(dato10));
+console.log(typeof(dato11));
